2566c538764f93e8f13840299c53642f2c5593f6
muhammadganny <mohammadganny@gmail.com>
2023-07-27 17:03:57 +0500

    bug fixed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import SearchComponent from './components/SearchComponent';
 import UserProfileComponent from './components/UserProfileComponent';
 
+
 const App = () => {
   const [user, setUser] = useState(null);
 
@@ -23,7 +24,7 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="main">
       <SearchComponent onSearch={handleSearch} />
       {user && <UserProfileComponent user={user} />}
     </div>
@@ -38,34 +39,4 @@ export default App;
 
 
 
-// import React from "react"
-// import './index.css';
-// import Git from "./Gitlog.png"
-// import Sgit from "./sgitlog.png"
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <div className="search">
-//         <h3>User Search</h3>
-//         <input
-//           type="text"
-//           className="searchbar"
-//         ></input>
-//       </div>
-//       <div className="card">
-//         <img src={Git} className="card-img"></img>
-//         <h3 className="name">Name </h3>
-//         <h4 className="email">email</h4>
-//         <br></br>
-//         <br></br>
-//         <button type="submit" className="bt">
-//           <img src={Git} className="btimg"></img>
-//         </button>
-//       </div>
-      
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,28 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 
 const SearchComponent = ({ onSearch }) => {
   const [searchText, setSearchText] = useState('');
+  const searchTextTimeoutRef = useRef();
 
-  useEffect(() => {
-    const searchTimeout = setTimeout(() => {
-      onSearch(searchText);
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setSearchText(value);
+
+    if (searchTextTimeoutRef.current) {
+      clearTimeout(searchTextTimeoutRef.current);
+    }
+
+    searchTextTimeoutRef.current = setTimeout(() => {
+      onSearch(value);
     }, 2000);
-
-    return () => clearTimeout(searchTimeout);
-  }, [searchText, onSearch]);
+  };
 
   return (
     <div className="search">
-        <h3>User Search</h3>
-        <input
-         type="text"
-         placeholder="Search GitHub User"
-         value={searchText}
-         onChange={(e) => setSearchText(e.target.value)}
-         className="searchbar"
-        ></input>
-  </div>
+      <h3>User Search</h3>
+      <input
+        type="text"
+        placeholder="Search GitHub User"
+        value={searchText}
+        onChange={handleInputChange} 
+        className="searchbar"
+      />
+    </div>
   );
 };
 
 export default SearchComponent;
+
